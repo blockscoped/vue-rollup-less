dac4b56ff286e77f333114c2450718d3636cf8ab
David Parker <david.parker@piksel.com>
2017-01-23 12:20:18 +0000

    Move from scss to less and fix process is not defined issue.

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -1,25 +1,36 @@
 import vue from 'rollup-plugin-vue2'
-import scss from 'rollup-plugin-scss'
+import less from 'rollup-plugin-less'
 import buble from 'rollup-plugin-buble'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify'
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
+import replace from 'rollup-plugin-replace'
+import json from 'rollup-plugin-json'
 
 const plugins = [
+  json(),
   vue(),
-  scss(),
-  buble({ exclude: 'node_modules/**' }),
+  less({output: 'dist/build.css'}),
+  buble({
+    objectAssign: 'Object.assign'
+  }),
   nodeResolve({ browser: true, jsnext: true }),
   commonjs()
 ]
 
 if (process.env.NODE_ENV === 'production') {
+  plugins.push(replace({
+    'process.env.NODE_ENV': JSON.stringify( 'production' )
+  }))
   plugins.push(uglify())
 }
 
 if (process.env.NODE_ENV === 'development') {
+  plugins.push(replace({
+    'process.env.NODE_ENV': JSON.stringify( 'development' )
+  }))
   plugins.push(livereload())
   plugins.push(serve({
     open: true
